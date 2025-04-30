88c5b9be9f5887c104b639f93e7bc123f9f4d20e
Dan <132749807+DanJoshua1@users.noreply.github.com>
2025-04-30 10:01:31 +0800

    render update

--- web_django/settings.py
+++ web_django/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,20 +9,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'l8e(i06@oey9vl4%3m67!0-p!4k0&1$l@w*9g1eeda3+1=!835')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = [
     '127.0.0.1',
-'bahaykapihan.netlify.app',
-'.netlify.app'    
+    'localhost',
+    '.onrender.com', 
 ]
-
+# Database configuration
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
 # Static files configuration
 STATIC_URL = '/static/' 
 STATICFILES_DIRS = [ 
     os.path.join(BASE_DIR, 'cafe', 'static'), 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Security settings
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Add whitenoise middleware
 MIDDLEWARE = [
